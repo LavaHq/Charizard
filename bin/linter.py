2b6a8c4e87289e484721ed1f1eeb08269ccbc686
Daniel Habib <d.g.habib7@gmail.com>
2015-09-29 22:59:47 -0400

    Update linter.py

--- bin/linter.py
+++ bin/linter.py
@@ -7,7 +7,7 @@ import sys
 
 from pylint.reporters.text import TextReporter
 from io import StringIO
-sys.path.append('/var/charizard/src')
+sys.path.append('/var/charizard')
 
 # check if pylint is installed and import it
 try:
