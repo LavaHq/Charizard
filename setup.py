7a902d9f3cb4f3ccc78fd672156a89627c29d59a
demoleas21 <andrew.demoleas@gmail.com>
2015-10-17 21:04:17 -0400

    Setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = 'Charizard',
+	version = '1.0',
+	description = 'A python API built on top of Chassis',
+	author = 'Hawt-Lava',
+	packages = ['Charizard', 'makefile', 'requirements.txt']
+	)
